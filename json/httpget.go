5ff6fcc1470b1a78037d035846c35813598d6f1d
fcj <chunjian@2008.sina.com>
2017-09-22 01:33:13 -0700

    处理Get请求响应的JSON示例

---
+++ json/httpget.go
@@ -0,0 +1,41 @@
+// 代码改编自《GO语言实战》
+// 并参考自https://studygolang.com/articles/4335
+// 处理Get请求响应的JSON示例
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type gResponse struct {
+	GcardNumber   uint64 `json:"card_number"`
+	GcardBalance  string `json:"card_balance"`
+	GbalanceTime  string `json:"balance_time"`
+	GcardValidity string `json:"card_validity"`
+	GcurrentTime  string `json:"current_time"`
+}
+
+func main() {
+	uri := "http://api.oupag.com/dev/api/shenzhentong.php?cardno=29444"
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	// 将JSON响应解码到结构类型
+	var gr gResponse
+	err = json.NewDecoder(resp.Body).Decode(&gr)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+
+	fmt.Printf("%#v", gr)
+}
