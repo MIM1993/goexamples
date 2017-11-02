7476834f06c7795e3410ce5c68e1ec4b3f63de17
fcj <chunjian@2008.sina.com>
2017-11-02 15:32:37 +0800

    补充Flags

--- cobra/main.go
+++ cobra/main.go
@@ -19,19 +19,29 @@ http://blog.csdn.net/qq_27809391/article/details/54089774
 package main
 
 import (
+	"fmt"
 	"github.com/fengchunjian/goexamples/cobra/node"
 	"github.com/spf13/cobra"
 	"os"
 )
 
+var versionFlag bool
+
 var mainCmd = &cobra.Command{
 	Use: "command",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.HelpFunc()(cmd, args)
+		if versionFlag {
+			fmt.Println("目前版本为1.0.0")
+		} else {
+			cmd.HelpFunc()(cmd, args)
+		}
 	},
 }
 
 func main() {
+	mainFlags := mainCmd.PersistentFlags()
+	mainFlags.BoolVarP(&versionFlag, "version", "v", false, "打印系统版本")
+
 	mainCmd.AddCommand(node.Cmd())
 	if mainCmd.Execute() != nil {
 		os.Exit(1)
